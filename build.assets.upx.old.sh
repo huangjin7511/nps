781d7a5dc3da9960d11ab611ffa168decfe770de
huangjin7511 <54235335+huangjin7511@users.noreply.github.com>
2025-03-27 14:56:12 +0800

    Update build.assets.upx.old.sh

--- build.assets.upx.old.sh
+++ build.assets.upx.old.sh
@@ -5,12 +5,6 @@ sudo apt-get install gcc-mingw-w64-i686 gcc-multilib
 env GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_sdk.dll cmd/npc/sdk.go
 env GOOS=linux GOARCH=386 CGO_ENABLED=1 CC=gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_sdk.so cmd/npc/sdk.go
 tar -czvf npc_sdk_old.tar.gz npc_sdk.dll npc_sdk.so npc_sdk.h
-
-curl -k -L -o "/opt/st/alist-linux-musl-mipsle.tar.gz" "https://github.com/AlistGo/alist/releases/download/$tag/alist-linux-musl-mipsle.tar.gz" 
-tar -xzvf /opt/st/alist-linux-musl-mipsle.tar.gz -C /opt/st
-tar -xvf /opt/st/upx-3.95-amd64_linux.tar.xz -C /opt
-chmod 777 /opt/upx-3.95-amd64_linux/upx
-chmod 777 /opt/st/strip
           
 CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
 /opt/st/strip npc.exe
