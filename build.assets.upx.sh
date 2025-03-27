75e271ce089630edee1d4397ab4dd8774e58bdbf
huangjin7511 <54235335+huangjin7511@users.noreply.github.com>
2025-03-27 14:47:55 +0800

    Create build.assets.upx.sh

---
+++ build.assets.upx.sh
@@ -0,0 +1,212 @@
+export GOPROXY=direct
+
+CURRENT_GO_VERSION=$(go version | awk '{print $3}' | sed 's/go//')
+go mod edit -go=$CURRENT_GO_VERSION
+go mod tidy
+echo "Updated go.mod to Go version: $CURRENT_GO_VERSION"
+
+sudo apt-get update
+sudo apt-get install gcc-mingw-w64-i686 gcc-multilib
+env GOOS=windows GOARCH=386 CGO_ENABLED=1 CC=i686-w64-mingw32-gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_sdk.dll cmd/npc/sdk.go
+env GOOS=linux GOARCH=386 CGO_ENABLED=1 CC=gcc go build -ldflags "-s -w -extldflags -static -extldflags -static" -buildmode=c-shared -o npc_sdk.so cmd/npc/sdk.go
+tar -czvf npc_sdk.tar.gz npc_sdk.dll npc_sdk.so npc_sdk.h
+
+curl -k -L -o "/opt/st/alist-linux-musl-mipsle.tar.gz" "https://github.com/AlistGo/alist/releases/download/$tag/alist-linux-musl-mipsle.tar.gz" 
+tar -xzvf /opt/st/alist-linux-musl-mipsle.tar.gz -C /opt/st
+tar -xvf /opt/st/upx-3.95-amd64_linux.tar.xz -C /opt
+chmod 777 /opt/upx-3.95-amd64_linux/upx
+chmod 777 /opt/st/strip
+          
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static"  ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf linux_amd64_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf linux_386_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+CGO_ENABLED=0 GOOS=freebsd GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf freebsd_386_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf freebsd_amd64_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+CGO_ENABLED=0 GOOS=freebsd GOARCH=arm go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf freebsd_arm_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf linux_arm_v7_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf linux_arm_v6_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf linux_arm_v5_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf linux_arm64_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=mips64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf linux_mips64_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=mips64le go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf linux_mips64le_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf linux_mipsle_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf linux_mips_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc.exe
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc.exe
+tar -czvf windows_386_client.tar.gz npc.exe conf/npc.conf conf/multi_account.conf
+
+
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc.exe
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc.exe
+tar -czvf windows_amd64_client.tar.gz npc.exe conf/npc.conf conf/multi_account.conf
+
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf darwin_amd64_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
+/opt/st/strip npc
+/opt/upx-3.95-amd64_linux/upx --lzma --best npc
+tar -czvf darwin_arm64_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf linux_amd64_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf linux_386_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf linux_arm_v5_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf linux_arm_v6_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf linux_arm_v7_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf linux_arm64_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+
+CGO_ENABLED=0 GOOS=freebsd GOARCH=arm go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf freebsd_arm_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+
+CGO_ENABLED=0 GOOS=freebsd GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf freebsd_386_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+
+CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf freebsd_amd64_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf linux_mips_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=mips64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf linux_mips64_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=mips64le go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf linux_mips64le_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf linux_mipsle_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf darwin_amd64_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps
+tar -czvf darwin_arm64_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+
+
+
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps.exe
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps.exe
+tar -czvf windows_amd64_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps.exe
+
+
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
+/opt/st/strip nps.exe
+/opt/upx-3.95-amd64_linux/upx --lzma --best nps.exe
+tar -czvf windows_386_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps.exe
